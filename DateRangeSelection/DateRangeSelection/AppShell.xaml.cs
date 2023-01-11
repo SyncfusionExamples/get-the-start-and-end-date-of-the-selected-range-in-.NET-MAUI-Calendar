eaa1dd37e1c7bb33c41dc2093c933bf064844b33
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-11 19:34:11 +0530

    New KB sample added

---
+++ DateRangeSelection/DateRangeSelection/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DateRangeSelection;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ DateRangeSelection/DateRangeSelection/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DateRangeSelection;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
